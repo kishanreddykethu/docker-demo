e4165ced1c416daf20c3cfec1a7a003383e42ccf
Kishan Reddy <kishanreddy.kethu@gmail.com>
2019-07-13 18:33:55 +0530

    Commit for environment based config.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ COPY requirements.txt /usr/src/code/
 WORKDIR /usr/src/code
 RUN pip install -r requirements.txt
 COPY . /usr/src/code/
+ENV env prod
 CMD gunicorn --bind 0.0.0.0:8000 app:app
-
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def api_hello():
-    if 'name' in request.args:
-        return 'Hello ' + request.args['name']
-    else:
-        return 'Hello John Doe'
+    env = os.environ.get("env", "prod")
+    name = request.args.get("name", "John Doe")
+
+    return f"Hello {name} from '{env}'"
+
 
 if __name__ == '__main__':
     app.run()
